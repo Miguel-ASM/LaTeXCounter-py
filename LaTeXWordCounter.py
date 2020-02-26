fcfd63057a99dc96dc251073f276eaef51f1694d
Miguel Ángel <19872243+Miguel-ASM@users.noreply.github.com>
2020-02-26 17:34:00 +0100

    fixup in goToStartDocument

--- LaTeXWordCounter.py
+++ LaTeXWordCounter.py
@@ -60,7 +60,7 @@ def goToStartDocument(handle):
 
     line = handle.readline().lstrip()
 
-    while not line.startswith(r'\begin{document}') and line > '':
+    while (not line.startswith(r'\begin{document}') ) and line > '':
         line = handle.readline().lstrip()
     return line
 
